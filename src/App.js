5f26b0f652ce0b8e70bcf4e3cc64f3e280c48368
Ritu <mearsgain@gmail.com>
2022-05-26 02:53:27 +0530

    dark first try

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import About from "./components/About";
 import "./App.css";
 
 let name="MEAR";
@@ -9,9 +10,10 @@ function App() {
     <>
 <Navbar title={"MEAR Texty"} aboutTexty="What do u want to know About us"/>
 {/* <Navbar/> */}
-<div className="container">
+<div className="container my-5">
 
-<TextForm heading="Enter the text to analyz"/>
+{/* <TextForm heading="Enter the text to analyz"/> */}
+<About />
 </div>
     </>
   );
